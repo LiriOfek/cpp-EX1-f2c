ea757a01b227cc5588e5d4249a775e0ad66e2ed7
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-14 12:02:03 +0300

    add main function that get temperature in Fahrenheit from user and convert it to Celsius

--- Ex1-f2c/main.cpp
+++ Ex1-f2c/main.cpp
@@ -5,7 +5,13 @@ Purpose: This file get temperature in Fahrenheit from user
 		 and convert it to Celsius
 \********************************************************/
 
+#include <iostream>
+
 const double INITIAL_TEMPERATURE = 0;
+const int SUCCESS = 0;
+const char* ENTER_TEMPERATURE = "Enter temperature in Fahrenheit:";
+const char* STRING_FAHRENHEIT = " Fahrenheit is: ";
+const char* STRING_CELSIUS = " Celsius";
 
 double f2c(double temperature_in_fahrenheit)
 {
@@ -24,4 +30,31 @@ double f2c(double temperature_in_fahrenheit)
 	temperature_in_celsius = (temperature_in_fahrenheit - 32) * 5 / 9;
 
 	return temperature_in_celsius;
+}
+
+int main()
+{
+	/**
+	* @brief  Get temperature in Fahrenheit from user and print the
+	*		  trmperature in Celsius
+	* @param  OUT SUCCESS in the end of the running
+	* @return  SUCCESS in the end of the running
+	* @author  Liri
+	*/
+	double temperature_in_fahrenheit = INITIAL_TEMPERATURE;
+	double temperature_in_celsius = INITIAL_TEMPERATURE;
+
+	/*receive temperature in Fahrenheit from user*/
+	std::cout << ENTER_TEMPERATURE << std::endl;
+	std::cin >> temperature_in_fahrenheit;
+
+	/*get temperature in Celsious*/
+	temperature_in_celsius = f2c(temperature_in_fahrenheit);
+
+	/*print temperature in Celsious*/
+	std::cout << temperature_in_fahrenheit <<
+		STRING_FAHRENHEIT << temperature_in_celsius
+		<< STRING_CELSIUS << std::endl;
+
+	return SUCCESS;
 }
