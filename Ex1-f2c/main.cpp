e77fb014cbb38681610c0e6b508bc7a663428a89
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-14 11:56:29 +0300

    add function that convert temperature in Fahrenheit to temperature in Celsius

--- Ex1-f2c/main.cpp
+++ Ex1-f2c/main.cpp
@@ -4,3 +4,24 @@ Author: Liri (14/4/2020)
 Purpose: This file get temperature in Fahrenheit from user
 		 and convert it to Celsius
 \********************************************************/
+
+const double INITIAL_TEMPERATURE = 0;
+
+double f2c(double temperature_in_fahrenheit)
+{
+	/**
+	* @brief  Get temperature in Fahrenheit, and return the
+	*		  temperature in Celsius
+	* @param  In double temperature_in_fahrenheit - temperature in
+	*	      Fahrenheit
+	*		  OUT double temperature_in_celsius - temperature in
+	*		  Celsius
+	* @return  The temperature in celsius
+	* @author  Liri
+	*/
+	double temperature_in_celsius = INITIAL_TEMPERATURE;
+
+	temperature_in_celsius = (temperature_in_fahrenheit - 32) * 5 / 9;
+
+	return temperature_in_celsius;
+}
