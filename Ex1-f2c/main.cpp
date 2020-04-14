c6940220eea7d539b15d9d4dbc04a73720415e5e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-14 11:54:24 +0300

    add main file with documentation

---
+++ Ex1-f2c/main.cpp
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (14/4/2020)
+Purpose: This file get temperature in Fahrenheit from user
+		 and convert it to Celsius
+\********************************************************/
